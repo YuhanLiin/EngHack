c6e2a396c502c56b7e72c7cdf3433c13dd34f5c4
Faisal Naqaweh <f.naqaweh@gmail.com>
2017-05-27 02:31:57 -0400

    State machine update, still mostly incomplete

--- EngHack/src/application/EnemySm.java
+++ EngHack/src/application/EnemySm.java
@@ -1,23 +1,61 @@
 package application;
 
-public class EnemySm {
+import game.Game;
+
+public class EnemySm extends CharacterSm {
 	
-	public EnemySm(Game environment) {
-		state = FSM.spawned;
-		gotHit = false;
+	enum enemyState {SPAWNED, movingLeft, movingRight, movingDown, movingUp, DEAD};
+	enum initialPos {bottomRight, bottomLeft, topRight, topLeft}
+
+	
+	enemyState myEnemyFSMState = enemyState.SPAWNED;
+	initialPos myEnemyInitialPos = initialPos.bottomRight;
+	
+	int positionX;
+	int positionY;
+	int maxX;
+	int minX;
+	int maxY;
+	int minY;
+	boolean gotHit;
+	
+	public void EnemySm() {
+	
+		switch(myEnemyFSMState) {
+			case SPAWNED:
+				switch(myEnemyInitialPos) {
+					case bottomRight: 
+						myEnemyFSMState = enemyState.movingLeft;
+						break;
+					case bottomLeft:
+						myEnemyFSMState = enemyState.movingRight;
+						break;
+					case topRight:
+						myEnemyFSMState = enemyState.movingDown;
+						break;
+					case topLeft:
+						myEnemyFSMState = enemyState.movingDown;
+						break;
+				}
+				break;
+			case movingRight:
+				moveRight();
+				break;
+			case movingLeft:
+				moveLeft();
+				break;
+			case movingDown:
+				moveDown();
+				break;
+			case movingUp:
+				moveUp();
+				break;
+			case DEAD:
+				getKilled();
+				break;
+		}
 	}
 	
-	enum FSM {spawned, topLeft, topRight, bottomLeft, bottomRight, dead};
-	
-	private int positionX;
-	private int positionY;
-	private int maxX;
-	private int minX;
-	private int maxY;
-	private int minY;
-
-	private boolean gotHit;
-	private FSM state;
 	
 	public void moveRight() {
 		positionX++;
@@ -30,5 +68,8 @@ public class EnemySm {
 	}
 	public void moveDown() {
 		positionY--;
+	}
+	public void getKilled() {
+		
 	}
 }
