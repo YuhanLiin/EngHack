717b3d22a1ece2039a871cb7e1f0b8250caa3085
yuhan lin <yuhan lin>
2017-05-27 00:55:58 -0400

    Made attack class

---
+++ EngHack/src/hitbox/Attack.java
@@ -0,0 +1,15 @@
+package hitbox;
+
+public abstract class Attack {
+	public boolean isDone = false;
+	public int hitId;
+	public Hitbox hitbox;
+	CharacterSm character;
+	
+	public Attack(CharacterSm c, int x, int y, int h, int w){
+		hitbox = new Hitbox(x, y, h, w);
+		character = c;
+	}
+	
+	public abstract void hitEffect(CharacterSm character); 
+}
