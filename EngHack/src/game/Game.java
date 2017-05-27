866bd5c816a049fbd3d404f4a7c187dcd0c8e24d
yuhan lin <yuhan lin>
2017-05-27 02:26:50 -0400

    Added runFrame

--- EngHack/src/game/Game.java
+++ EngHack/src/game/Game.java
@@ -1,10 +1,26 @@
 package game;
+import java.util.ArrayList;
+
+import application.*;
 
 public class Game {
-	public final int height;
-	public final int width;
-	public ArrayList<CharacterSm> enemies = ArrayList<CharacterSm>();
+	public final int height = 600;
+	public final int width = 800;
+	public ArrayList<EnemySm> enemies = ArrayList<CharacterSm>();
 	public ArrayList<Attack> playerAttacks = ArrayList<Attack>();
 	public ArrayList<Attack> enemyAttacks = ArrayList<Attack>();
-	public CharacterSm player;
+	public PlayerSm player;
+	public int frameCount = 0;
+	
+	public Game(){
+		player = new PlayerSm(this, 300, 400, 100, 100);
+		
+	}
+	
+	public void runFrame(){
+		frameCount++;
+		if (frameCount % (60*5) == 0){
+			enemies.add(new EnemySm());
+		}
+	}
 }
