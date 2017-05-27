a625dcd6692290154b96fc12013d8ee9b488b945
yuhan lin <yuhan lin>
2017-05-27 00:25:12 -0400

    Made hibox file

---
+++ EngHack/src/hitbox/Hitbox.java
@@ -0,0 +1,23 @@
+package hitbox;
+
+public class Hitbox {
+	public int x;
+	public int y;
+	int height;
+	int width;
+	
+	public Hitbox(int px, int py, int h, int w){
+		x = px;
+		y = py;
+		height = h;
+		width = w;
+	}
+	
+	public boolean collide(Hitbox hitbox){
+		if (Math.abs(x - hitbox.x) < width/2 + hitbox.width/2
+			&& Math.abs(y - hitbox.y) < height/2 + hitbox.height/2){
+			return true;
+		}
+		return false;
+	}
+}
